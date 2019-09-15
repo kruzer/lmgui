b7b2abebb696a183e72ed3883b38a9191e070989
mikey <mikey@3lance.pl>
2019-09-15 22:52:50 +0200

    version info

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, VERSION } from '@angular/core';
+import { ConfigService } from '../config.service';
 
 @Component({
   selector: 'app-about',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+  constructor(private configService: ConfigService) { }
+  private AngularVersion: string;
+  private BoostVersion: string;
+  private AppVersion: string;
 
   ngOnInit() {
+    this.AngularVersion = VERSION.full;
+    this.getVersion();
   }
+
+  getVersion() {
+    this.configService.getConfig()
+      .subscribe((data: any) => {
+        this.BoostVersion = data.config.boostVersion,
+        this.AppVersion = data.config.appVersion;
+      });
+    }
 
 }
